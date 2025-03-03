280cca45d3237ee37008fd808e87019fe20fe8aa
Guilherme Carvalho <guicarvcontato@outlook.com>
2025-03-03 11:54:40 -0300

    exemplo de codigo aula 3 - operadores e expressões

---
+++ aula-3-operadores-e-expressoes/script.js
@@ -0,0 +1,47 @@
+// OPERADORES ARITMÉTICOS
+
+console.log(5 + 3);
+console.log(5 - 3);
+console.log(5 * 3);
+console.log(5 / 3);
+console.log(5 % 3);
+
+// OPERADORES DE ATRIBUIÇÃO
+
+let num = 5;
+
+num += 3;
+console.log(num);
+
+num -= 2;
+console.log(num);
+
+num *= 2;
+console.log(num);
+
+num /= 3;
+console.log(num);
+
+num %= 2;
+console.log(num);
+
+// OPERADOR DE ENCADEAMENTO
+
+let nome = "guilherme"
+let idade = 21
+
+console.log(nome.idade)
+
+// OPERADORES DE INCREMENTO E DECREMETO
+
+let count = 0;
+
+console.log(count++);
+console.log(count);
+
+console.log(++count);
+
+console.log(count--);
+console.log(count);
+
+console.log(--count);

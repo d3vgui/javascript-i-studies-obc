eb62a0d949ce3d7f4e6d5b21a61746a61a2f91ef
Guilherme Carvalho <guicarvcontato@outlook.com>
2025-03-03 11:43:02 -0300

    exemplo de codigo aula 1 - tipos de dados

---
+++ aula-1-tipos-de-dados/script.js
@@ -0,0 +1,30 @@
+// NUMBER
+
+console.log(typeof 3);
+console.log(typeof 45);
+console.log(typeof 76);
+console.log(typeof 23);
+console.log(typeof 12);
+console.log(typeof 9);
+
+
+// BIGINT
+
+console.log(typeof 1234567890123456789012345678901234567890n)
+
+
+// STRING
+
+console.log(typeof "guilherme")
+console.log("guilherme".length)
+
+
+// BOOLEAN
+
+console.log(typeof true);  
+console.log(typeof false);
+
+// NULL E UNDEFINED
+
+console.log(typeof null)
+console.log(typeof undefined);

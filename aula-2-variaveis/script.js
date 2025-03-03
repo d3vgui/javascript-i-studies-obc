e52574f142da0d00b123c780bee989d4a9f71f27
Guilherme Carvalho <guicarvcontato@outlook.com>
2025-03-03 11:46:50 -0300

    exemplode codigo aula 2 - variaveis

---
+++ aula-2-variaveis/script.js
@@ -0,0 +1,16 @@
+// VAR
+
+var nome = "guilherme"
+
+console.log(nome)
+
+// LET
+
+let idade = 21
+
+console.log(idade)
+
+// CONST
+
+const profissao = "desenvolvedor fullstack"
+console.log(profissao)

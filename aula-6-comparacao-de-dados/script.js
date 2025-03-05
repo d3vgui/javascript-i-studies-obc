6a47d19e48262d2d6d1c55a419a3bd2743cdce54
Guilherme Carvalho <guicarvcontato@outlook.com>
2025-03-04 22:23:37 -0300

    exemplo de codigo aula 6 - comparação de dados

---
+++ aula-6-comparacao-de-dados/script.js
@@ -0,0 +1,32 @@
+
+// COMPARAÇÃO DE IGUALDADE
+
+console.log(3 == 3) // true 
+console.log(4 == 3) // false
+
+let calc = 3 * 3 + 1
+console.log(calc = 10) // true
+
+console.log(10 === 10) // true
+console.log(10 === 11) // false
+
+
+// ---------------------------------------------------- //
+
+// COMPARAÇÃO DE DESIGUALDADE
+
+console.log(3 != 3) // false
+console.log(5 != 3) // true
+console.log(5 != "5") // false
+console.log(5 !== "5") // true
+
+// ---------------------------------------------------- //
+
+// COMPARADORES MATEMÁTICOS
+
+console.log(4 > 3) // true
+console.log(5 > 10) // true
+console.log(5 >= 10) // true
+console.log(5 >= 5) // true
+console.log(6 <= 7) // true
+console.log(6 < 6) // false

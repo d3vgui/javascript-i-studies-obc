2c395145faa828f05b624fed54b51a09e18c7571
Guilherme Carvalho <guicarvcontato@outlook.com>
2025-03-04 21:44:35 -0300

    exemplo de codigo aula 5 - recursos nativos do navegador

---
+++ aula-5-recursos-nativos-do-navegador/script.js
@@ -0,0 +1,11 @@
+let nome = 'guilherme'
+
+console.log(nome)
+console.error("deu algum erro")
+console.warn("cuidado")
+
+// window.alert("Olá, mundo!")
+// window.prompt("Insira o seu nome:")
+// let confirmacao = window.confirm("Confirma essa ação?")
+
+alert("Hello, world!")

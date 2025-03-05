e133890b24d45c7fedeecd7e85f7a5bd086360a0
Guilherme Carvalho <guicarvcontato@outlook.com>
2025-03-04 22:28:08 -0300

    exemplo de codigo aula 7 - operadores logicos

---
+++ aula-7-operadores-logicos/script.js
@@ -0,0 +1,18 @@
+// OPERADOR E (AND)
+
+console.log(true && true) // true
+console.log(true && false) // false
+console.log(6 > 7 && 8 === 9) // false
+console.log(6 < 7 && 8 !== 9)
+
+// ------------------------------------------------ //
+
+// OPERADOR OU (OR)
+
+console.log("guilherme" !== "pedro" || 9 < 5) // true
+
+// ------------------------------------------------ //
+
+// OPERADOR NÃO (NOT)
+
+console.log(!9 < 5 === true) // true
